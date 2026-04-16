7891f1e64d052f89b239390c828d0f0c9917f937
Chalfton <zachjdevore@hotmail.com>
2026-04-16 17:20:47 -0400

    Set up the structure for the project, I think this is correct it's been years.

---
+++ src/main/java/com/oop_group/banking_application/BankApplication.java
@@ -0,0 +1,19 @@
+package com.oop_group.banking_application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class BankApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(BankApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
---
+++ src/main/java/com/oop_group/banking_application/Launcher.java
@@ -0,0 +1,9 @@
+package com.oop_group.banking_application;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(BankApplication.class, args);
+    }
+}
